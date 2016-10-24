4e7ca1ea5886ff90461cf8a4829f0d2a0a7728c4
douglasjosenieves <douglasjosenieves@gmail.com>
2016-10-24 16:11:34 -0300

    Clientes editar status
    Clientes Status editar

--- interprise/envios/form-fichas-contacto-editar.php
+++ interprise/envios/form-fichas-contacto-editar.php
@@ -211,6 +211,18 @@ $resul2 = mysql_query($qry2);
 }
 
 
+if ($status=='CLIENTE') {
+
+
+
+	$qry2 = "INSERT INTO chat (`id_para`, `id_de`, `mensaje`, `fecha_envio`, `ip`) SELECT id , '".$editado_por."', '".'Cliente Nuevo: Id:'.$referencia.' Nombre:'.$nombres.' '.$apellidos."', '".$fechaphp."', '".$ip."' FROM usuarios where anulado <> 1 and tipo = 'administrador';";
+ 
+
+$resul2 = mysql_query($qry2);
+}
+
+
+
 //echo $qry;
 
 
