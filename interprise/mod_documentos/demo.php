814d92fb69f1b2e2d6d3c85906daa80362f2fffd
Mi casa <douglasjosenieves@gmail.com>
2017-02-14 16:00:47 -0400

    Cambios en elo modulos de documentacion
    cambias

--- interprise/mod_documentos/demo.php
+++ interprise/mod_documentos/demo.php
@@ -2,75 +2,137 @@
 <html>
 <head>
 	<title>Prueba</title>
+<style type="text/css">
+	
+	body {
+
+font-size: 11px;
+text-align: justify;
+
+	}
+.titulo {
+
+	font-size: 14px;
+	font-weight: 800;
+	margin: 1px;
+}
+
+
+.titulo p {
+
+	 
+	margin: 1px;
+}
+.titulo2 p {
+
+	 
+	margin: 1px;
+}
+
+.titulo2 {
+
+	font-size: 18px;
+	font-weight: 800;
+		margin: 1px;
+}
+hr {
+	    height: 6px;
+    background-color: #dcdcdc;
+    position: relative;
+     
+    top: 60px;
+    z-index: -50;
+    border: none;
+}
+
+#firma {
+
+
+
+}
+
+ 
+</style>
+
+  <style>
+     @page { margin: 180px 50px; }
+     #header { position: fixed; left: 0px; top: -150px; right: 0px; height: 150px;  text-align: center; }
+     #footer { position: fixed; left: 0px; bottom: -180px; right: 0px; height: 150px;   }
+     #footer .page:after { content: counter(page, upper-roman); }
+   </style>
 </head>
 <body>
+ 
+  <div id="header">
+     <hr>
+<img src="images/logo-light.png">
+   </div>
+   <div id="footer">
+     <p class="page">Pagina <?php $PAGE_NUM ?></p>
+    <hr style="background-color: #a21414;
+    top: 1px;">
+     <center>
+     <p style="font-size: 9;font-weight: 800;">Contacto: 0034 91 436 13 33 Cohen y Aguirre Lobby Solutions. Madrid, España. 2014. Para rectificación de datos, contactad a info@cohenyaguirre.es / Velázquez 27, Planta 1, puerta Izquierda. Madrid, España.</p>
+</center>
+   </div>
 
 
-
-
-<p>&nbsp;</p>
-<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
-<p>&nbsp;</p>
+<div class="titulo">
+<center>
 <p><strong>Cohen</strong> <strong>y Aguirre Lobby Solutions</strong></p>
+<div class="titulo2">
 <p><strong>-Contrato</strong> <strong>de</strong> <strong>Estudio</strong> <strong>Due Dilligence-</strong></p>
-<p><strong>&nbsp;</strong></p>
-<p>&nbsp;</p>
-<p><strong>D/D&ntilde;a. Alheliska Rodriguez con CI, DNI, NIF o Pasaporte n&uacute;mero 11.920.165, con domicilio en Av. Prado Humbold, Calle Rio Ticoporo, Res. Alessandra, piso 3, Apto 3b, Prados del Este, La Ciudadela., con n&uacute;mero de tel&eacute;fono: 2129793163, y direcci&oacute;n electr&oacute;nica <a href="mailto:alheliska@gmail.com">alheliska@gmail.com</a>, manifiesta su inter&eacute;s por contratar un informe de an&aacute;lisis a IBEROAMERICANA BROKERS CONSULTING, S.L con CIF n&uacute;mero B87209615, en adelante, Cohen y Aguirre Lobby Solutions, con domicilio en Calle Vel&aacute;zquez, n&uacute;mero 27, planta 1, puerta izquierda, 28001, Madrid, Espa&ntilde;a.</strong></p>
-<p><strong>&nbsp;</strong></p>
-<p><strong>Dicho estudio se realizar&aacute; con base en la informaci&oacute;n obtenida por Registros P&uacute;blicos sobre &ldquo;Sector Restauraci&oacute;n&rdquo;:</strong></p>
-<p>&nbsp;</p>
+</div>
+</center>
+</div>
+
+
+<p><strong>D/D&ntilde;a. <?php  echo $don ?> con CI, DNI, NIF o Pasaporte n&uacute;mero <?php  echo $pasaporte ?>, con domicilio en <?php  echo $direccion ?>., con n&uacute;mero de tel&eacute;fono: <?php echo  $tel ?>, y direcci&oacute;n electr&oacute;nica <a href="mailto:<?php  echo $email ?>"><?php  echo $email ?></a>, manifiesta su inter&eacute;s por contratar un informe de an&aacute;lisis a IBEROAMERICANA BROKERS CONSULTING, S.L con CIF n&uacute;mero B87209615, en adelante, Cohen y Aguirre Lobby Solutions, con domicilio en Calle Vel&aacute;zquez, n&uacute;mero 27, planta 1, puerta izquierda, 28001, Madrid, Espa&ntilde;a.</strong></p>
+<p><strong>Dicho estudio se realizar&aacute; con base en la informaci&oacute;n obtenida por Registros P&uacute;blicos sobre &ldquo;Sector <?php  echo $sector ?>:</strong></p>
 <ul>
-<li><strong>228 (Bar de tapas)</strong><strong>.</strong></li>
+<li><strong><?php  echo $refOpcion ?></strong><strong>.</strong></li>
 </ul>
-<p>&nbsp;</p>
-<p><strong>Cohen</strong> <strong>y Aguirre Lobby Solutions percibir&aacute; en concepto de realizaci&oacute;n y an&aacute;lisis el importe de 750&euro; m&aacute;s el correspondiente IVA 21% <?php echo $monto ?>&euro;,&nbsp; &nbsp;formando un total de &nbsp;907.50&euro;.</strong></p>
-<p>&nbsp;</p>
+<p><strong>Cohen</strong> <strong>y Aguirre Lobby Solutions percibir&aacute; en concepto de realizaci&oacute;n y an&aacute;lisis el importe de <?php  echo $importe ?>&euro; m&aacute;s el correspondiente IVA 21% <?php  echo $iva ?>&euro;, formando un total de <?php  echo $total ?>&euro;.</strong></p>
 <p><strong>Dicho</strong> <strong>abono se realizar&aacute; a nombre de IBEROAMERICANA BROKERS CONSULTING, S.L., en la cuenta del Banco Sabadell BSABESBB ES59 0081 5519 0500 0114 7716. El resguardo se enviar&aacute; a </strong><a href="mailto:contabilidad@cohenyaguirre.es"><strong>contabilidad@cohenyaguirre.es</strong></a> <strong>para surtir de efecto a este acuerdo.</strong></p>
-<p><strong>&nbsp;</strong></p>
-<p><strong>En relaci&oacute;n a la cl&aacute;usula CUARTA del contrato Acuerdo de Servicio de fecha de 14 de Noviembre de 2016, por el que se solicitan nuestros servicios, el</strong> <strong>Contratante</strong> <strong>reconoce</strong> <strong>honorarios</strong> <strong>de</strong> <strong>10%</strong> <strong>sobre</strong> <strong>el</strong> <strong>importe</strong> <strong>resultante</strong> <strong>de</strong> <strong>inversi&oacute;n</strong> <strong>de</strong> <strong>este</strong> <strong>Estudio</strong> <strong>a Cohen &amp; Aguirre Lobby Solutions. </strong></p>
-<p>&nbsp;</p>
+<p><strong>En relaci&oacute;n a la cl&aacute;usula CUARTA del contrato Acuerdo de Servicio de fecha de <?php  echo $fechac ?>, por el que se solicitan nuestros servicios, el</strong> <strong>Contratante</strong> <strong>reconoce</strong> <strong>honorarios</strong> <strong>de</strong> <strong>10%</strong> <strong>sobre</strong> <strong>el</strong> <strong>importe</strong> <strong>resultante</strong> <strong>de</strong> <strong>inversi&oacute;n</strong> <strong>de</strong> <strong>este</strong> <strong>Estudio</strong> <strong>a Cohen &amp; Aguirre Lobby Solutions. </strong></p>
 <p><strong>De</strong> <strong>la misma forma, se reconoce un Acuerdo de Confidencialidad entre las partes, en relaci&oacute;n a la cl&aacute;usula S&Eacute;PTIMA donde se proh&iacute;be a todos los efectos la reproducci&oacute;n, cesi&oacute;n y representaci&oacute;n de dicho estudio a personas no autorizadas por Cohen &amp; Aguirre Lobby Solutions. Dicho acuerdo de confidencialidad ha sido le&iacute;do y comprendido en su totalidad y por esto se procede a contratar y firmar la aceptaci&oacute;n del encargo de dicho Estudio.</strong></p>
-<p>&nbsp;</p>
 <p><strong>Sobre el Acuerdo de Confidencialidad: </strong></p>
-<p>&nbsp;</p>
 <p><strong>PRIMERO.-</strong> Nuestro Convenio con El Vendedor objeto del negocio de estudio requiere de la firma de un Acuerdo de No Divulgaci&oacute;n y de la obtenci&oacute;n de una evidencia de su capacidad financiera, antes de publicar el nombre y la ubicaci&oacute;n de su negocio. Esta informaci&oacute;n se mantendr&aacute; en forma confidencial.</p>
-<p>&nbsp;</p>
 <p><strong>SEGUNDO.-</strong> Cohen &amp; Aguirre Lobby Solutions y el firmante de este contrato con respecto al negocio en venta, entendemos y acordamos lo siguiente:</p>
-<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
 <p>(i) Que toda informaci&oacute;n suministrada sobre el negocio es sensible y confidencial y que su divulgaci&oacute;n a terceros puede causar da&ntilde;os al negocio y a sus propietarios.</p>
 <p>(ii) No se permite divulgar ninguna informaci&oacute;n relacionada con el negocio a una tercera parte que no haya firmado tambi&eacute;n dicho Acuerdo de No Divulgaci&oacute;n. La &ldquo;Informaci&oacute;n&rdquo; utilizada en este Acuerdo incluir&aacute; el hecho de que los negocios est&aacute;n en venta, adem&aacute;s de otros datos suministrados.</p>
-<p>(iii) No contactar a los propietarios del negocio o sus arrendadores, empleados, proveedores o clientes, excepto a trav&eacute;s IBEROAMERICANA BROKERS CONSULTING, S.L. (y su marca Cohen &amp; Aguirre Lobby Solutions).&nbsp; Toda la correspondencia, las investigaciones, las ofertas y las negociaciones relacionadas con la compra o el arrendamiento de cualquier negocio mostrado por IBEROAMERICANA BROKERS CONSULTING, S.L. (y su marca Cohen &amp; Aguirre Lobby Solutions) ser&aacute;n llevadas a cabo exclusivamente a trav&eacute;s del mismo.</p>
+<p>(iii) No contactar a los propietarios del negocio o sus arrendadores, empleados, proveedores o clientes, excepto a trav&eacute;s IBEROAMERICANA BROKERS CONSULTING, S.L. (y su marca Cohen &amp; Aguirre Lobby Solutions). Toda la correspondencia, las investigaciones, las ofertas y las negociaciones relacionadas con la compra o el arrendamiento de cualquier negocio mostrado por IBEROAMERICANA BROKERS CONSULTING, S.L. (y su marca Cohen &amp; Aguirre Lobby Solutions) ser&aacute;n llevadas a cabo exclusivamente a trav&eacute;s del mismo.</p>
 <p>(iv) Entendemos que si llegamos a ocupar el cargo de gerente u otro cargo relacionado con cualquiera de los negocios mostrados u ofrecidos a nosotros para la venta, o si adquirimos, publicitamos, arrendamos o intercambiamos cualquiera de los negocios mostrados, entonces, deberemos abonar los honorarios en concepto de indemnizaci&oacute;n por el incumplimiento de la presente obligaci&oacute;n.</p>
-<p>&nbsp;</p>
-<p><strong>&nbsp;</strong></p>
-<p><strong>&nbsp;</strong></p>
-<p><strong>&nbsp;</strong></p>
-<p><strong>&nbsp;</strong></p>
-<p><strong>&nbsp;</strong></p>
-<p><strong>&nbsp;</strong></p>
-<p><strong>TERCERO.-</strong> Que toda la informaci&oacute;n relacionada con los negocios en venta proviene de El Vendedor o de otras fuentes y que no se ofrece garant&iacute;a alguna en cuanto a la precisi&oacute;n, veracidad o exactitud de dicha informaci&oacute;n m&aacute;s all&aacute; de la provista en Registro Mercantil y Declaraciones de Impuestos.&nbsp; Aceptamos que Cohen &amp; Aguirre Lobby Solutions, no es responsable de la precisi&oacute;n de ninguna informaci&oacute;n que hayamos recibido Cohen &amp; Aguirre Lobby Solutions, y a su personal excluidos de cualesquiera reclamaci&oacute;n o da&ntilde;o que pudieran surgir a causa de la ineficiencia, inexactitud o deficiencia de cualquier informaci&oacute;n que nos sea suministrada u obtengamos por nuestros medios, en relaci&oacute;n con cualquier operaci&oacute;n en que pudi&eacute;ramos intervenir.</p>
-<p>&nbsp;</p>
+<p><strong>TERCERO.-</strong> Que toda la informaci&oacute;n relacionada con los negocios en venta proviene de El Vendedor o de otras fuentes y que no se ofrece garant&iacute;a alguna en cuanto a la precisi&oacute;n, veracidad o exactitud de dicha informaci&oacute;n m&aacute;s all&aacute; de la provista en Registro Mercantil y Declaraciones de Impuestos. Aceptamos que Cohen &amp; Aguirre Lobby Solutions, no es responsable de la precisi&oacute;n de ninguna informaci&oacute;n que hayamos recibido Cohen &amp; Aguirre Lobby Solutions, y a su personal excluidos de cualesquiera reclamaci&oacute;n o da&ntilde;o que pudieran surgir a causa de la ineficiencia, inexactitud o deficiencia de cualquier informaci&oacute;n que nos sea suministrada u obtengamos por nuestros medios, en relaci&oacute;n con cualquier operaci&oacute;n en que pudi&eacute;ramos intervenir.</p>
 <p><strong>CUARTO.-</strong> En el caso de que celebremos un contrato de compra de un negocio, suministraremos un estado financiero y un historial personal y comercial, y autorizamos a El Comprador a que obtenga, por todos los medios permitidos en Derecho toda la informaci&oacute;n crediticia y financiera sobre la compa&ntilde;&iacute;a que presentamos.</p>
-<p>&nbsp;</p>
 <p><strong>QUINTO.-</strong> Que en relaci&oacute;n con los datos personales referentes al Vendedor que nos pudieran ser suministrados asumimos las siguientes obligaciones:</p>
-<p>&nbsp;</p>
 <p>(i) Tratar los datos de car&aacute;cter personal &uacute;nica y exclusivamente conforme a las instrucciones que se reciban expresamente de Cohen &amp; Aguirre Lobby Solutions;</p>
 <p>(ii) No destinar y/o utilizar los datos de car&aacute;cter personal para cualquier otro fin distinto al expresamente indicado por Cohen &amp; Aguirre Lobby Solutions para el cumplimiento de los servicios contratados, o de forma que suponga un incumplimiento de las instrucciones expresas que se proporcione;</p>
 <p>(iii) No revelar, transferir, ceder o de otra forma comunicar los datos de car&aacute;cter personal de El Vendedor, ni siquiera para su conservaci&oacute;n, a ning&uacute;n tercero;</p>
 <p>(iv) Implementar, en su caso, las medidas de seguridad que sean de aplicaci&oacute;n conforme a lo dispuesto en el Real Decreto 1720/2007, de 21 de diciembre por el que se aprueba el Reglamento de Desarrollo de la Ley Org&aacute;nica 15/1999.</p>
 <p>(v) Destruir o devolver a Cohen &amp; Aguirre Lobby Solutions (seg&uacute;n &eacute;ste le indique) la totalidad de los datos de car&aacute;cter personal, as&iacute; como cualesquiera copias o soportes en los que &eacute;stos estuviesen contenidos, cuando as&iacute; sea requerido para ello.</p>
-<p>&nbsp;</p>
-<p><strong>SEXTO.-</strong> Reconocemos que hemos recibido una copia fiel y exacta del presente Acuerdo, que lo hemos le&iacute;do cuidadosamente y que lo entendemos plenamente, en MADRID, a fecha 03 de Febrero de 2017.</p>
-<p>&nbsp;</p>
+<p><strong>SEXTO.-</strong> Reconocemos que hemos recibido una copia fiel y exacta del presente Acuerdo, que lo hemos le&iacute;do cuidadosamente y que lo entendemos plenamente, en MADRID, a fecha <?php  echo $hoy; ?>.</p>
 <p><strong>COMENTARIOS:</strong> ESTUDIO A PRESENTAR &ndash; FRANQUICIA / NEGOCIO.</p>
-<p>&nbsp;</p>
 <p><strong>Declaraci&oacute;n de Protecci&oacute;n </strong><strong>de Datos:</strong></p>
 <p><strong>De</strong> <strong>acuerdo</strong> <strong>con</strong> <strong>lo</strong> <strong>dispuesto</strong> <strong>en el Art. 5 de la Ley Org&aacute;nica 15/1999 de Protecci&oacute;n de Datos y el Art. 12 del RD 1720/2007 consiente expresamente que los datos facilitados a lo largo de la prestaci&oacute;n del servicio sean recogidos y tratados en un fichero titularidad de IBEROAMERICANA BROKERS CONSULTING SL con CIF B87209615, con domicilio en Calle Vel&aacute;zquez, n&uacute;mero 27, planta 1, puerta izquierda, 28001, Madrid, Espa&ntilde;a. La finalidad de dicho tratamiento es prestarle un &oacute;ptimo servicio como cliente y, en el caso de que lo desee, hacerle llegar informaci&oacute;n comercial por medio de Correo electr&oacute;nico, Canal M&oacute;vil, Telemarketing o Mailing, de los productos y servicios de la Compa&ntilde;&iacute;a y del resto de empresas colaboradoras, as&iacute; como de productos y servicios de terceros relacionados con los sectores de inversi&oacute;n, turismo, comercio exterior, venta y promoci&oacute;n de inmuebles.</strong></p>
-<p>&nbsp;</p>
-<p><strong>&nbsp;</strong></p>
-<p><strong>En</strong> <strong>Madrid,</strong> <strong>se firma a d&iacute;a 03 del mes de Febrero de 2017.</strong></p>
-<p>&nbsp;</p>
-<p><strong>Contratantes</strong><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Cohen </strong><strong>y Aguirre</strong></p>
-<p><strong>Lobby Solutions</strong></p>
+<p><strong>En</strong> <strong>Madrid,</strong> <strong>se firma a d&iacute;a <?php  echo $hoy; ?>.</strong></p>
+
+
+
+<br> 
+<div id="firma" style="font-size: 14px;font-weight: 800;">
+<div id="l" style="height: 50px;"  >
+<strong>Contratantes</strong>
+</div>
+
+<div id="r" align="right">
+
+<p><strong>Cohen y Aguirre Lobby Solutions</strong></p>
+ 
+<img src="images/firma.jpg" width="" height="">
+
+</div>
+
+</div>
 </body>
 </html>
--- interprise/mod_documentos/demo_sys.php
+++ interprise/mod_documentos/demo_sys.php
@@ -5,7 +5,30 @@ use Dompdf\Dompdf;
 
 // instantiate and use the dompdf class
 $dompdf = new Dompdf();
+$don= 'Douglas Nieves';
+$pasaporte = '000000002132415';
+$direccion = 'Guatire PArque Alto';
+$tel = '04141331946';
+$email = 'douglasjosenieves@gmail.com';
+$fecha_contratacion= '2017-02-01';
+$date = new DateTime($fecha_contratacion);
 
+
+
+
+$sector = 'Restauración';
+$refOpcion= '0001 Restaurant';
+
+$importe = '20';
+$iva= '10';
+$total='30';
+$dias = array("Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sábado");
+$meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+ 
+$hoy =  $dias[date('w')]." ".date('d')." de ".$meses[date('n')-1]. " del ".date('Y') ;
+
+$fechac= $dias[$date->format("w")]." ".$date->format("d")." de ".$meses[$date->format("n")-1]. " del ".$date->format("Y") ;
+//Salida: Viernes 24 de Febrero del 2012
 ob_start();
 $monto = '1.000.000';
 require_once 'demo.php';
